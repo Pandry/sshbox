b8892017efaf797d8d7e62eed85e9e9ca7a6b8da
Pandry <17499836+Pandry@users.noreply.github.com>
2022-03-19 18:24:24 +0100

    First working k8s integration

--- main.go
+++ main.go
@@ -9,13 +9,15 @@ import (
 	"io"
 	"log"
 	"path/filepath"
+	"time"
 
 	"github.com/gliderlabs/ssh"
-	appsv1 "k8s.io/api/apps/v1"
 	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/remotecommand"
 	"k8s.io/client-go/util/homedir"
 	// github.com/spf13/viper
 )
@@ -38,7 +40,7 @@ func main() {
 		panic(err)
 	}
 
-	deploymentsClient := clientset.AppsV1().Deployments(apiv1.NamespaceDefault)
+	podsClient := clientset.CoreV1().Pods(apiv1.NamespaceDefault)
 
 	ssh.Handle(func(s ssh.Session) {
 		//io.WriteString(s, "Hello world\n")
@@ -50,34 +52,23 @@ func main() {
 		if err != nil {
 			fmt.Println(err.Error())
 		}
+		// check if pods alredy exists
+		// 		check it's not in terminating state/ is in running state
 		userKeyFingerprint := hex.EncodeToString(hash.Sum(nil))
 		io.WriteString(s, "Hi "+s.User()+"! This is your fingerprint: "+userKeyFingerprint+"\n") // Sarebbe bello aggiungere un punto al secondo finché il deployment non è completo
+		podName := "sshbox-" + s.User() + "-" + userKeyFingerprint
 
-		deployment := &appsv1.Deployment{
+		pod := &apiv1.Pod{
 			ObjectMeta: metav1.ObjectMeta{
-				Name: "demo-deployment",
+				Name:   podName,
+				Labels: map[string]string{"app": "sshbox"},
 			},
-			Spec: appsv1.DeploymentSpec{
-				Replicas: int32Ptr(1),
-				Selector: &metav1.LabelSelector{
-					MatchLabels: map[string]string{
-						"app": "sshbox",
-					},
-				},
-				Template: apiv1.PodTemplateSpec{
-					ObjectMeta: metav1.ObjectMeta{
-						Labels: map[string]string{
-							"app": "sshbox",
-						},
-					},
-					Spec: apiv1.PodSpec{
-						Containers: []apiv1.Container{
-							{
-								Name:  "sshbox-" + s.User() + userKeyFingerprint,
-								Image: "alpine",
-								TTY:   true,
-							},
-						},
+			Spec: apiv1.PodSpec{
+				Containers: []apiv1.Container{
+					{
+						Name:  "sshbox",
+						Image: "alpine",
+						TTY:   true,
 					},
 				},
 			},
@@ -85,21 +76,45 @@ func main() {
 
 		// Create Deployment
 		io.WriteString(s, "Creating deployment...\n")
-		_, err = deploymentsClient.Create(context.TODO(), deployment, metav1.CreateOptions{})
+		result, err := podsClient.Create(context.TODO(), pod, metav1.CreateOptions{})
 		if err != nil {
 			panic(err)
 		}
+		time.Sleep(2 * time.Second)
 		io.WriteString(s, "Deployment created! Redirecting...\n")
 
-		/*clientset.CoreV1().RESTClient().Post().
-		Resource("pods").Name(result.pod).
-		Namespace(result.Namespace).
-		SubResource("exec")*/
+		req := clientset.CoreV1().RESTClient().Post().Resource("pods").Name(result.Name).Namespace(result.Namespace).SubResource("exec")
+		req.VersionedParams(&apiv1.PodExecOptions{
+			TypeMeta:  metav1.TypeMeta{},
+			Stdin:     true,
+			Stdout:    true,
+			Stderr:    true,
+			TTY:       true,
+			Container: "sshbox",
+			Command:   []string{"sh"},
+		}, scheme.ParameterCodec)
+
+		exec, err := remotecommand.NewSPDYExecutor(config, "POST", req.URL())
+		if err != nil {
+			panic(err)
+		}
+
+		err = exec.Stream(remotecommand.StreamOptions{
+			Stdin:  s,
+			Stdout: s,
+			Stderr: s,
+		})
+
+		if err != nil {
+			io.WriteString(s, err.Error()+"\n")
+		}
+
+		io.WriteString(s, "Quitted!\n")
 
 		//defer deployment deletion
 		defer func() {
-			deploymentsClient.Delete(context.TODO(), deployment.GetObjectMeta().GetName(), metav1.DeleteOptions{})
-			fmt.Println("Deleting ", deployment.GetObjectMeta().GetName())
+			podsClient.Delete(context.TODO(), pod.GetObjectMeta().GetName(), metav1.DeleteOptions{})
+			fmt.Println("Deleting ", pod.GetObjectMeta().GetName())
 		}()
 	})
 
@@ -110,5 +125,3 @@ func main() {
 	log.Fatal(ssh.ListenAndServe(":2222", nil, publicKeyOption))
 
 }
-
-func int32Ptr(i int32) *int32 { return &i }
